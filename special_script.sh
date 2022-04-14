fcb56908e5316872112b560fcff07748f3261511
mashru020 <mashrurrahman16@gmail.com>
2022-04-15 00:11:36 +0600

    special parameter  and

--- special_script.sh
+++ special_script.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# $#-numbre of parameter and $0-script name
 if [[ $# -ne 2 ]]; then
 	echo "You didnot enter exactly 2 parameters"
 	echo "Usage: $0 <file1> <file2>"
 	exit 1
 fi
+
+# $@ and $* - unquoted both variable worked same, worded differently when quoted like "$@" and "$*"
+
+#touch "$@"
+echo "$@"
+
+#"$*" -> change IFS variable
+IFS=,
+echo "$*"
