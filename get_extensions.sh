9aed387cdba18c8c1592698474a27cdf86b96527
mashru020 <mashrurrahman16@gmail.com>
2022-04-15 12:16:00 +0600

    select functionality added in get_extention porject

--- get_extensions.sh
+++ get_extensions.sh
@@ -2,6 +2,24 @@
 
 read -p "What is your first name? 				: " firstname
 read -p "what is your surname/family name? 			: " surname
+
+echo "=== phone types ==="
+PS3="Which type of  phone you would like ot use?		: "
+select phone in headset handheld;
+do
+#echo "Phone type: $phone"
+break
+done
+echo
+
+echo "=== Departments ==="
+PS3="Which department you are from?				: "
+select department in finance sales "customer service" engineering;
+do
+#echo "Department :$department"
+break
+done
+
 read -N 4 -p "what is your extension number? (Must be 4 digits)	: " extension
 echo
 read -N 4 -s -p "What access code would you like to use when dialing in?(Must be 4 digits)	: " access
@@ -9,10 +27,12 @@ echo
 echo
 
 
-echo "Frst Name: $firstname"
-echo "Surname  : $surname"
-echo "Extension: $extension"
-echo "Pin      : $access"
+echo "Frst Name : $firstname"
+echo "Surname   : $surname"
+echo "Phone type: $phone"
+echo "Department: $department"
+echo "Extension : $extension"
+echo "Pin       : $access"
 
 
-echo "$firstname,$surname,$extension,$access" >> extension.csv
+echo "$firstname,$surname,$phone,$department,$extension,$access" >> extension.csv
