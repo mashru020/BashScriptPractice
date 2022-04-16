7f2ed97ab63f2f6f6d9d2e543439f825534b243b
mashru020 <mashrurrahman16@gmail.com>
2022-04-16 20:23:33 +0600

    if statement

--- logic/if.sh
+++ logic/if.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-if [ 2 -gt 1 ]; then
-	echo test passed true
-fi
+#if [ 2 -gt 1 ]; then
+#	echo test passed true
+#fi
 
-if [ 1 -gt 2 ]; then
-	echo test passed false
-fi
+#if [ 1 -gt 2 ]; then
+#	echo test passed false
+#fi
 
 read -p "Enter your name : " name
 if [ $name = "mashru" ]; then
 	echo welcome $name
 else
 	echo sorry! $name
+fi
+
+
+read -p "Enter a number (1 to 9) : " number
+if [ $number -gt 5 ]; then
+        echo too large
+elif [ $number -lt 5 ]; then
+	echo too small
+else
+        echo congratulations!
 fi
